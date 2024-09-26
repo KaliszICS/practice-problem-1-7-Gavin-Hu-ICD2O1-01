09a2ae1661ae0fda5c19d8d59f0e2253d4792841
Gavin-Hu-ICD2O1-01 <gavin.hu@student.tdsb.on.ca>
2024-09-26 13:28:23 +0000

    commt

--- PP1_7.py
+++ PP1_7.py
@@ -1,19 +1,41 @@
 
 
-q1():
+def q1():
   #Write code here
+ print(True)
+  
 
-q2():
-  #Write code here
 
-q3():
+def q2():
   #Write code here
+  user_input = input("Input an integer: ")
+  number = int(user_input)
+  result = number > 5
+  print(result)
+  print("false")
 
-q4():
+def q3():
   #Write code here
+  user_input = input("Input the letter a: ")
+  result = user_input == 'a'
+  print(result)
 
-q5():
+def q4():
   #Write code here
+  user_input = input("Input a word earlier in the dictionary than google: ")
+  result = user_input < 'google'
+  print(result)
+
+def q5():
+  #Write code here
+  first_input = input("Input an integer: ")
+  second_input = input("Input another integer: ")
+  num1 = int(first_input)
+  num2 = int(second_input)
+  product = num1 * num2
+  result = product > 40
+  print("Your numbers multiplied together are greater than 40:", result)
+  print("false")
 
 #Do edit the code below
 #Comment the lines below when running your tests
